b25a2f6f8ec740ba8af58749cf16c5cd2c7a8f7e
Tom Gregor <tgregor@ivsoftware.com>
2025-09-07 16:40:43 -0600

    ORIGIN maui sketchpad

---
+++ GlyphProvider.Demo.Maui/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace GlyphProvider.Demo.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new MainPage();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            if (Application.Current is not null && DeviceInfo.Platform == DevicePlatform.WinUI)
+            {
+                var window = base.CreateWindow(activationState);
+                window.Width = 450;
+                window.Height = 850;
+
+                // Ensure window resizing completes
+                window.Dispatcher.DispatchAsync(() => { }).GetAwaiter().OnCompleted(() =>
+                {
+                    var disp = DeviceDisplay.Current.MainDisplayInfo;
+                    window.X = (disp.Width / disp.Density - window.Width) / 2;
+                    window.Y = (disp.Height / disp.Density - window.Height) / 2;
+                });
+                return window;
+            }
+            else
+            {
+                return base.CreateWindow(activationState);
+            }
+        }
+    }
+}
---
+++ GlyphProvider.Demo.Maui/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using IVSoftware.Portable;
+
+namespace GlyphProvider.Demo.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            if (count % 2 == 0)
+            {
+                var cycleCount = count / 2;
+                CounterBtn.FontFamily = "OpenSans-Regular";
+                if (cycleCount == 1)
+                    CounterBtn.Text = $"Cycled {cycleCount} time";
+                else
+                    CounterBtn.Text = $"Cycled {cycleCount} times";
+                // CounterBtn.WidthTracksHeight = false;
+            }
+            else
+            {
+                CounterBtn.FontFamily = "onepage-icons";
+
+                //    // This is what the Glyph property abstracts!
+                //    // OLD
+                //    // CounterBtn.Glyph = GlyphProvider.FromFontConfigJson(Framework.OP_FONT_FAMILY)["Search"];
+                //    // NEW
+                //    CounterBtn.Glyph = Framework.OP_FONT_FAMILY.ToGlyph(StdOnepageIconsGlyph.Search);
+                // Alt
+                var xaml = CounterBtn.FontFamily.ToGlyph(StdBasicsIcons.Search, GlyphFormat.Xaml);
+                // Readable
+                var display = CounterBtn.FontFamily.ToGlyph(StdBasicsIcons.Search, GlyphFormat.UnicodeDisplay);
+                { }
+
+                //    CounterBtn.WidthTracksHeight = true;
+                //}
+
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+        }
+    }
+}

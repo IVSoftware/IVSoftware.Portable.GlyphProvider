34351b8faa01fcf8b6b892b4459963265e5c652b
Tom Gregor <tgregor@ivsoftware.com>
2025-09-09 18:08:11 -0600

    MAUI is BFTD

--- GlyphProvider.Demo.Maui/MauiProgram.cs
+++ GlyphProvider.Demo.Maui/MauiProgram.cs
@@ -13,7 +13,7 @@ namespace IVSGlyphProvider.Demo.Maui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                    fonts.AddFont("icon-basics.ttf", "icon-basics");
+                    //fonts.AddFont("icon-basics.ttf", "icon-basics");
                     //fonts.AddEmbeddedResourceFont(typeof(IVSoftware.Portable.GlyphProvider).Assembly,"basics-icons.ttf", "basics-icons")  ;
                 });
 
